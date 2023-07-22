b7fa2bfa47878128eda2457d7fa5d84dff6f4096
Paul Ndalila <paulndalila001@gmail.com>
2023-07-22 09:48:46 +0300

    admin panel update

---
+++ admin/app.js
@@ -0,0 +1,9 @@
+const profileWindow = document.getElementById("user-profile");
+
+function closeWindow(){
+    profileWindow.style.display = "none";
+}
+
+function openWindow(){
+    profileWindow.style.display = "flex";
+}
