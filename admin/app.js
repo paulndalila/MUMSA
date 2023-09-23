85866dcab14d96a98f269d1df8836ca602d2a46f
Paul Ndalila <paulndalila001@gmail.com>
2023-09-23 12:48:33 +0300

    admin fix

--- admin/app.js
+++ admin/app.js
@@ -1,9 +1,25 @@
 const profileWindow = document.getElementById("user-profile");
+const newsWindow = document.getElementById("news-add");
+const eventWindow = document.getElementById("event-add");
 
-function closeWindow(){
+function closeProfileWindow(){
     profileWindow.style.display = "none";
 }
 
-function openWindow(){
+function openProfileWindow(){
     profileWindow.style.display = "flex";
+}
+
+function closeNewsWindow(){
+    newsWindow.style.display = "none";
+}
+function openNewsWindow(){
+    newsWindow.style.display = "flex";
+}
+
+function closeEventWindow(){
+    eventWindow.style.display = "none";
+}
+function openEventWindow(){
+    eventWindow.style.display = "flex";
 }
